38b9b0effec43da2055b6a835b8f4b69c8e77be2
arunmohandas196 <arun.mohandas@blacklane.com>
2020-06-08 13:09:53 +0200

    Initial commit

---
+++ application/src/main/java/com/akqa/meeting/application/port/out/MeetingCalendarOutputPort.java
@@ -0,0 +1,7 @@
+package com.akqa.meeting.application.port.out;
+
+import com.akqa.meeting.application.domain.MeetingCalendar;
+
+public interface MeetingCalendarOutputPort {
+  void outputCalendar(MeetingCalendar meetingCalendar);
+}
